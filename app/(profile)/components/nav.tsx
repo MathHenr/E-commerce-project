36df82f9d94c62e09d59ac26a0ed3e83507eda6f
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-11-07 19:55:05 -0300

    Initial commit in profile_page branch

--- app/(profile)/components/nav.tsx
+++ app/(profile)/components/nav.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image"
+
 import { UserIcon } from "@/components/user-icon"
 
 export const Nav = () => {
     return (
         <nav className="w-full p-4 flex items-center justify-between font-poppins">
-            <h1 className="text-lg">
-                Hello, Username!
-                <p className="text-sm text-slate-700/90">
-                    Tell more about you right here
-                </p>
-            </h1>
+            <div className="flex items-center gap-6">
+                <Image height={140} width={140} src="/logo.svg" alt="Logo" />
+                <h1 className="text-lg">
+                    Hello, Username!
+                    <p className="text-sm text-slate-700/90">
+                        Tell more about you right here
+                    </p>
+                </h1>
+            </div>
             <div>
                 <UserIcon />
             </div>
