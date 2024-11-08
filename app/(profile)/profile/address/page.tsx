a310cded45178e21dce36a7b0ff0a7ad697e9047
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-11-08 08:44:25 -0300

    .

--- app/(profile)/profile/address/page.tsx
+++ app/(profile)/profile/address/page.tsx
@@ -1,32 +1,3 @@
-// import Image from "next/image"
-
-// import { getServerSession } from "next-auth"
-
-// export default async function Page () {
-//     const session = await getServerSession()
-    
-//     if (!session) {
-//         return (
-//             <div>
-//                 User not authorized
-//             </div>
-//         )
-//     }
-    
-//     return (
-//         <div>
-//             <p>
-//                 Profile Page {session.user?.name}
-//             </p>
-//             <Image width={120} height={120} src={session.user?.image ? session.user.image : '/unknown-user.svg'} alt="User" />
-//             <div>
-//                 <span>CEP</span>
-//                 <input type="text" />
-//             </div>
-//         </div>
-//     )
-// }
-
 "use client"
 
 import { useState } from "react";
